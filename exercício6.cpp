7a409eeec91310b7e352e82f664a4e4838cb1177
eduardopaiva0 <paiva.eduardo@aluno.ifsp.edu.br>
2025-04-30 19:58:52 -0300

    Add files via upload

---
+++ exercício6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace	std;
+
+int main(){
+	double num1, num2, mediaGeo;
+	cout << "Digite o primeiro numero: ";
+	cin >> num1;
+	cout << "Digite o segundo numero: ";
+	cin >> num2;	
+	
+	mediaGeo = sqrt(num1 * num2);
+	
+	cout << "A media geometrica dos valores digitados eh: " << mediaGeo;
+}
